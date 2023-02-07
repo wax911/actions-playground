7bc8ff9d07bc7ccaecbdd0ee06c5ea63242e7be5
Maxwell Mapako <wax911@users.noreply.github.com>
2023-02-07 21:35:28 +0200

    [feature] update test-sum with failing test

--- test-sum.py
+++ test-sum.py
@@ -5,7 +5,7 @@ from .main import sum
 class TestSum(unittest.TestCase):
 
     def test_sum(self):
-        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+        self.assertEqual(sum([1, 2, 3]), 7, "Should be 7")
 
     def test_sum_tuple(self):
         self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
